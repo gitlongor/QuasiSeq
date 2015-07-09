3303c3c296899faae901347dcb66fd1b84989c8d
Long Qu <long.qu@wright.edu>
2015-07-09 10:15:34 -0400

    dir struct

---
+++ prg/template/header.R
@@ -0,0 +1,28 @@
+base.dir=file.path('C:/Users/Longor/Desktop/work/XXXXXXXXXXX')
+out.dir=file.path(base.dir, 'out')
+prg.dir=file.path(base.dir, 'prg')
+dat.dir=file.path(base.dir, 'dat')
+img.dir=file.path(base.dir, 'img')
+
+{
+library(parallel)
+}
+
+prg.name=paste('XXXXXXXXXXXXX', Sys.Date(), sep='-')
+img.name=file.path(img.dir, paste(prg.name, 'RData', sep='.'))
+out.file=function(suffix, subdir) if(missing(subdir)) file.path(out.dir, paste(prg.name, suffix, sep='_')) else file.path(out.dir, subdir, paste(prg.name, suffix, sep='_')) 
+
+setwd(out.dir)
+
+options(contrasts=c('contr.sum', 'contr.sum'))
+
+.sessionInfo=list(sessionInfo=sessionInfo(), Sys.info=Sys.info(),
+  Sys.getenv=Sys.getenv(names=TRUE), capabilities=capabilities(),
+  options=options(), RNGkind=RNGkind(), .libPaths=.libPaths(), 
+  date=date(), wd=getwd())
+
+cl=makeCluster(8L)
+	clusterEvalQ(cl, {; options(contrasts=c('contr.sum', 'contr.sum'))})
+
+####################################### END OF HEADER ################################################
+	
