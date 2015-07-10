b22d9df8d7930b279a9fb91697d3b3ae3972dbdf
Long Qu <long.qu@wright.edu>
2015-07-10 13:30:10 -0400

    uploaded new R header

--- prg/template/header.R
+++ prg/template/header.R
@@ -1,14 +1,22 @@
-base.dir=file.path('C:/Users/Longor/Desktop/work/XXXXXXXXXXX')
+base.dir=file.path('C:/Users/Long/XXXXXXXXXXXXXXXXXXXXXXXXX')
 out.dir=file.path(base.dir, 'out')
 prg.dir=file.path(base.dir, 'prg')
 dat.dir=file.path(base.dir, 'dat')
 img.dir=file.path(base.dir, 'img')
 
-{
-library(parallel)
+if(TRUE){
+	library(parallel)
+	RNGkind("L'Ecuyer-CMRG")
+	cl=makeCluster(8L)
+	clusterEvalQ(cl, {
+		RNGkind("L'Ecuyer-CMRG"); 
+		options(contrasts=c('contr.sum', 'contr.sum'))
+	})
+	clusterSetRNGStream(cl , 9992722L)
 }
+set.seed(9992722L)
 
-prg.name=paste('XXXXXXXXXXXXX', Sys.Date(), sep='-')
+prg.name=paste('XXXXXXXXXXXXXXXXXXXXXXXXX', Sys.Date(), sep='_')
 img.name=file.path(img.dir, paste(prg.name, 'RData', sep='.'))
 out.file=function(suffix, subdir) if(missing(subdir)) file.path(out.dir, paste(prg.name, suffix, sep='_')) else file.path(out.dir, subdir, paste(prg.name, suffix, sep='_')) 
 
@@ -19,10 +27,8 @@ options(contrasts=c('contr.sum', 'contr.sum'))
 .sessionInfo=list(sessionInfo=sessionInfo(), Sys.info=Sys.info(),
   Sys.getenv=Sys.getenv(names=TRUE), capabilities=capabilities(),
   options=options(), RNGkind=RNGkind(), .libPaths=.libPaths(), 
-  date=date(), wd=getwd())
+  date=date(), wd=getwd(),.Random.seed=.Random.seed)
 
-cl=makeCluster(8L)
-	clusterEvalQ(cl, {; options(contrasts=c('contr.sum', 'contr.sum'))})
 
 ####################################### END OF HEADER ################################################
 	
