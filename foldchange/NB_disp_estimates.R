3e7b1a64df5f50c2d23fb1618570d591070a41f6
Long Qu <long.qu@wright.edu>
2015-07-16 13:39:46 -0400

    partial fold change simulation

---
+++ foldchange/NB_disp_estimates.R
@@ -0,0 +1,53 @@
+print(getwd())
+setwd("C:/Users/Klirk/Desktop/Simulation_Code/Simulation_Code/KIRC_Simulations/bartlett")
+### Load Bioconductor Packages
+require(edgeR)
+
+### Load CRAN Packages
+require(SimSeq)
+
+### Load Data
+RNGkind("Mersenne-Twister", "Inversion")
+set.seed(156165L)
+data(kidney); 
+kidney$counts=kidney$counts[sample(nrow(kidney$counts)),]
+counts <- kidney$counts
+treatment <- kidney$treatment
+
+### Set simulation variables
+filter.mean <- 0 # lower bound of average read count for simulated genes
+filter.nonzero <- 1 # lower bound for nonzero read counts for simulated genes
+
+### Remove low count genes
+keep.counts <- ( rowMeans(counts) >= filter.mean ) & ( rowSums(counts > 0) >= filter.nonzero )
+counts <- counts[keep.counts, ]
+
+### Subset down to just tumor counts for dispersion estimation in tumor group
+counts.tumor <- counts[ , treatment == "Tumor"]
+
+### Use edgeR package's tagwise dispersion estimate
+### Calculate tagwise estimates from edgeR. Not that edgeR uses the
+### parameterizion: Var(y) = \mu + \omega * \mu^2 where E(Y) = \mu and
+### \omega is a dispersion parameter for the NB model.
+y <- DGEList(counts = counts.tumor)
+y <- calcNormFactors(y)
+y <- estimateDisp(y)
+nbdisp.tumor <- y$tagwise.dispersion
+
+### Subset down to just non-tumor counts for dispersion estimation in non-tumor group
+counts.nontumor <- counts[ , treatment == "Non-Tumor"]
+
+### Calculate tagwise estimates from edgeR. Not that edgeR uses the
+### parameterizion: Var(y) = \mu + \omega * \mu^2 where E(Y) = \mu and
+### \omega is a dispersion parameter for the NB model.
+y <- DGEList(counts = counts)
+y <- calcNormFactors(y)
+y <- estimateDisp(y)
+nbdisp.nontumor <- y$tagwise.dispersion
+
+### Save Results
+saveRDS(nbdisp.tumor, file = "nbdisp_tumor_edgeR.RDS")
+saveRDS(nbdisp.nontumor, file = "nbdisp_nontumor_edgeR.RDS")
+
+
+save.image('NB_disp_estimates.RData')
