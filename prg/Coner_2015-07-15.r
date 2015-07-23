53931a7da6b2d15b2238770e0e47b3901073b955
RamajeyamTharshan <tharshan10684@gmail.com>
2015-07-23 20:49:42 +0530

    included Hessian,optim,nlminb

--- prg/Coner_2015-07-15.r
+++ prg/Coner_2015-07-15.r
@@ -16,11 +16,13 @@ if(F){
 }
 set.seed(9992722L)
 
-prg.name=paste('Conver', Sys.Date(), sep='_')
+prg.name=paste('Coner', Sys.Date(), sep='_')
 img.name=file.path(img.dir, paste(prg.name, 'RData', sep='.'))
 out.file=function(suffix, subdir) if(missing(subdir)) file.path(out.dir, paste(prg.name, suffix, sep='_')) else file.path(out.dir, subdir, paste(prg.name, suffix, sep='_')) 
 
 setwd(out.dir)
+
+library(QuasiSeq)
 
 options(contrasts=c('contr.sum', 'contr.sum'))
 
@@ -47,19 +49,15 @@ my_LBNB<-function(y, mu,phi,tau){
 	ans = exp(		
 		numerator-lbeta(y, term1)-term3
 	)/y
-	ans[y==0]=exp(numerator-term3)
+	y0=which(y==0)
+	ans[y0]=exp(numerator-term3)[y0]
 	ans
 }
 
 		
 ##################END####################################
 
-#This is the function to find exp(h(y,tau,pi))=H,where 
-#exp(h(y,tau=t,pi))=integration(f(QLNB)-f(LBNB)) over the mean values
-#f(QLNB)=exp(1/pi)*((1/(1+t*x))^(1/t)*(t*x/(t*x+1))^y)
-#f(LBNB)=(exp(lgamma(y+x*(t*pi+1)/(pi-1))+lbeta((x*(t*pi+1))/(pi-1)
-#             +(t*(2*pi-1)+1)/(t*(pi-1)),y+1/t)-log(factorial(y))-lgamma(x*(t*pi+1)/(pi-1))
-#             -lbeta((t*(2*pi-1)+1)/t*(pi-1),1/t)))
+
 
 get.h<-function(phi,y,tau){
 	f = function(mu) my_QLNB(y, mu, phi, tau)
@@ -71,39 +69,46 @@ get.h<-function(phi,y,tau){
 
 get.h(2,10,0.2)
 
-logLikQNB=function(y, mu, phi, tau)
+LikQNB=function(y, mu, phi, tau)
 {
 	h=get.h(phi, y, tau)
 	my_QLNB(y, mu, phi, tau) * h
 }
-logLikQNB = Vectorize(logLikQNB, c('phi','y','tau'))
+LikQNB = Vectorize(log(LikQNB), c('phi','y','tau'))######
 
 
 #Simulation
-my.simu<-function(mu,phi,tau) {
-alpha<-(tau*(2*phi-1)+1)/(tau*(phi-1))
-beta<-1/tau
-r=mu*(alpha-1)/beta
-p<-sort(rbeta(100,alpha,beta))
-y<-rnbinom(100,r,prob=p)
-library(MASS)
-model<-glm.nb(y~p)
-S<-simulate(model,nsim=5)
-return(S)
+my.simu<-function(n, mu,phi,tau) {
+	alpha<-(tau*(2*phi-1)+1)/(tau*(phi-1))
+	beta<-1/tau
+	r=mu*(alpha-1)/beta
+	p<-sort(rbeta(n,alpha,beta))
+	y=rnbinom(n,r,prob=p)
+	attr(y, 'phi')=phi
+	attr(y, 'tau')=tau
+	attr(y, 'mu')=mu
+	y
 }
 
-my.simu(10,2,.5)
+my.simu(8, 10,2,.5)
 
+jpeg(file = "plot10.jpg")
+curve(log(likQNB(y=10000, mu=x, phi=1555.1, tau=.9)), 8, 12,main="y=10000,phi=1555.1,tau=0.9",xlab="mean",ylab="Log likelihood")
+curve(log(my_LBNB(y=10000, mu=x, phi=1555.1, tau=.9)), 8, 12, add=T, col=4, lty=3, lwd=3,main="y=10000,phi=1555.1,tau=0.9",xlab="mean",ylab="Log likelihood")
+legend("topright", c("LogLQNB","LogLBNB"), cex=0.8,col=c("black","blue"),pch=21:22,lwd=2:3)
+dev.off()
 
-curve(log(logLikQNB(y=10, mu=x, phi=1.1, tau=.5)), 8, 12)
-curve(log(my_LBNB(y=10, mu=x, phi=1.1, tau=.5)), 8, 12, add=T, col=4, lty=3, lwd=3)
-curve(log(logLikQNB(y=10, mu=x, phi=1.1, tau=.5)), 200, 2000)
+jpeg(file = "plot11.jpg")
+curve(log(likQNB(y=10, mu=x, phi=1.1, tau=.5)), 200, 2000)
 curve(log(my_LBNB(y=10, mu=x, phi=1.1, tau=.5)), 200, 2000, add=T, col=4, lty=3, lwd=3)
+legend("topright", c("LogLQNB","LogLBNB"), cex=0.8,col=c("black","blue"),pch=21:22,lwd=2:3)
 abline(v=10)
+dev.off()
 
 
 curve(log(logLikQNB(y=0, mu=x, phi=1.1, tau=.5)), 0, 20)
 curve(log(my_LBNB(y=0, mu=x, phi=1.1, tau=.5)), 0, 20, add=T, col=4, lty=3, lwd=3)
+
 curve(log(logLikQNB(y=0, mu=x, phi=1.1, tau=.5)), 200, 2000)
 curve(log(my_LBNB(y=0, mu=x, phi=1.1, tau=.5)), 200, 2000, add=T, col=4, lty=3, lwd=3)
 
@@ -115,7 +120,7 @@ curve(log(my_LBNB(y=1000, mu=x, phi=1.1, tau=.5)), 0, 500, add=T, col=4, lty=3, 
 save.image(img.name)
 
 #Score functions comparison with mean
-my_BNB_Score<-function(y,mu,phi,tau){
+my_BNB_Score1<-function(y,mu,phi,tau){
 	term1 = mu*(tau*phi+1)/(phi-1)
 	term2 = (tau*(2*phi-1)+1)/(tau*(phi-1))
     term3= term1+term2+y+1/tau
@@ -124,12 +129,94 @@ my_BNB_Score<-function(y,mu,phi,tau){
                    digamma(term3)-digamma(term1))
 ans
 }
-my_QNB_Score<-function(y, mu,phi,tau){
+my_QNB_Score1<-function(y, mu,phi,tau){
    (y-mu)/(phi*(mu+tau*mu^2))
 }
+jpeg(file = "plot28.jpg")
+curve(my_BNB_Score1(y=10, mu=x, phi=151.1, tau=0.5), 8, 12,col=4, lty=3, lwd=3,main="y=10,phi=151.1,tau=0.5",xlab="mean",ylab="Score")
+curve(my_QNB_Score1(y=10, mu=x, phi=151.1, tau=0.5), 8, 12, add=T,main="y=10,phi=151.1,tau=0.5",xlab="mean",ylab="Score")
+legend("topright", c("ScoreBNB","ScoreQNB"), cex=0.8,col=c("blue","black"),pch=21:22,lwd=3:4)
+abline(h=0)
+dev.off()
 
-curve(my_BNB_Score(y=10, mu=x, phi=1.1, tau=.5), 1, 20)
-curve(my_QNB_Score(y=10, mu=x, phi=1.1, tau=.5), 1, 20, add=T, col=4, lty=3, lwd=3)
+curve(my_BNB_Score1(y=0, mu=x, phi=1.1, tau=.5), 0, 1)
+curve(my_QNB_Score1(y=0, mu=x, phi=1.1, tau=.5), 0, 1, add=T, col=4, lty=3, lwd=3)
+abline(h=0)
+
+#Hessian value
+my_BNB_Hess<-function(y,mu,phi,tau){
+	term1 = mu*(tau*phi+1)/(phi-1)
+	term2 = (tau*(2*phi-1)+1)/(tau*(phi-1))
+    term3= term1+term2+y+1/tau
+	term4 = (tau*phi+1)/(phi-1)
+	ans = term4^2*(trigamma(y+term1)+trigamma(term1+term2)-
+                   trigamma(term3)-trigamma(term1))
+ans
+}
+
+bnbScore=function(mu, y, phi, tau)sum(my_BNB_Score1(y,mu,phi,tau))
+qnbScore=function(mu, y, phi, tau)sum(my_QNB_Score1(y,mu,phi,tau))
+bnbHess=function(mu, y, phi, tau)-sum(my_BNB_Hess(y,mu,phi,tau))
+bnbNegLogLik=function(mu, y, phi, tau)-sum(log(my_LBNB(y,mu,phi,tau)))
+
+#optim
+bnbMle=function(y, phi, tau){
+	par.hess=tryCatch(optim(max(1e-9,mean(y)), bnbNegLogLik, bnbHess,method='L-BFGS-B', lower=0, y=y, phi=phi, tau=tau,hessian=TRUE), error=function(...)NA_real_)
+    par=par.hess$par
+	hessian=par.hess$hessian
+	variance=mean(-hessian)
+	return(c(par,variance))
+	}
+
+qnbMle=function(y, phi, tau) mean(y)
+
+}	
+getBnbQnb=function(reps,n, mu, tau, phi)
+{replicate(reps,
+	c(BNB=bnbMle(my.simu(n, mu, phi, tau), phi, tau),
+      QNB=qnbMle(my.simu(n, mu, phi, tau), phi, tau)
+	)
+)
+}	
+
+
+#nlminb
+bnbMle1=function(y, phi, tau)
+nlminb(max(1e-9,mean(y)),bnbNegLogLik,bnbScore ,bnbHess ,lower=0,y=y,phi=phi, tau=tau)
+
+getBnbQnb1=function(reps,n, mu, tau, phi)
+{replicate(reps,
+	c(BNB=bnbMle1(my.simu(n, mu, phi, tau), phi, tau),
+	  QNB=qnbMle(my.simu(n, mu, phi, tau), phi, tau)
+	)
+)
+}
+
+	
+#simulation
+simulation=function(simu,n,mu,tau,phi)#simu=desired number of simulations  
+{replicate(simu,my.simu(n, mu, phi, tau))}
+
+
+##############################
+phis=seq(1+1e-3, 3, length=20)
+taus=seq(1e-3, 3, length=20)
+mus =seq(.1, 1e4, length=20)
+ns = c(2, 4, 8, 16, 32)
+cases=expand.grid(n=ns, mu=mus, tau=taus, phi=phis)
+reps=1e3L
+
+getBnbQnb=function(n, mu, tau, phi)
+{replicate(5,
+	c(BNB=bnbMle(my.simu(n, mu, phi, tau), phi, tau),
+	  QNB=qnbMle(my.simu(n, mu, phi, tau), phi, tau)
+	)
+)
+}
+getBnbQnb2=function(x) getBnbQnb(x[1],x[2],x[3],x[4])
+est = apply(cases, 1, getBnbQnb2)
+dim(est)=c(2L, reps, nrow(cases))
+
 
 save.image(img.name)
 
