c0d10b1ded17dc213be7ca66a83465cfd932021b
Long Qu <long.qu@wright.edu>
2015-10-30 20:14:00 +0530

    debugged log like and score function; added regression test cases

--- prg/bnbReg.R
+++ prg/bnbReg.R
@@ -10,7 +10,7 @@ mu <- exp(x%*%beta+o)
 	)/y
 	y0=which(y==0)
 	ans[y0]=exp(numerator-term3)[y0]
-	ans
+	sum(log(ans))
 }
 
 
@@ -33,7 +33,7 @@ My_score_Beta<-function(beta, y, x, o, tau, phi)
 	
 	sums=numeric(n)
 	for(i in seq_len(n)){
-		k=seq_len(y)-1L
+		k=seq_len(y[i])-1L
 		pos.denom=k+mu.tau.phip1.dphin1[i]
 		neg.denom=pos.denom+ t.2phin1p1.dtphin1.p1tau
 		sums[i] = sum(1/pos.denom-1/neg.denom)
@@ -45,20 +45,23 @@ My_score_Beta<-function(beta, y, x, o, tau, phi)
 	score
 }
 library(numDeriv)
-grad(function(beta)log(my_LBNB(beta=beta, y=10,x=matrix(1:3,nrow=1),o=1, phi=2, tau=.5)), matrix(1:3,nrow=3))
-My_score_Beta(matrix(1:3,nrow=3), y=10, x=matrix(1:3,nrow=1), o=1, tau=.5, phi=2)
+set.seed(234234)
+x=cbind(1, matrix(runif(10*3, 0, 1), nc=3))
+bet=runif(4, 0,1)
+xb=x%*%bet
+y=rnbinom(10, 1/(tau=2), mu=drop(exp(xb)))
 
-grad(function(beta)log(my_LBNB(beta=beta, y=0,x=matrix(1:3,nrow=1),o=1, phi=2, tau=.5)), matrix(1:3,nrow=3))
-My_score_Beta(matrix(1:3,nrow=3), y=0, x=matrix(1:3,nrow=1), o=1, tau=.5, phi=2)
+grad(my_LBNB, x=bet,,,, y=y, x,o=rep(0,10), phi=1+1e-3, tau=2)
+My_score_Beta(bet, y=y, x=x, o=rep(0,10), tau=2, phi=1+1e-3)
+
 
  
-My_diag_hess<-function(beta, y, x, o, tau, phi)
+My_hess_beta<-function(beta, y, x, o, tau, phi)
 {
 	n=NROW(x)
 	mu <- exp(x%*%beta+o)
 	mu.x<-as.vector(mu)*x
-	dmu2<-as.vector(mu)*(x*x)
-	#dmu2.dbi.dbj<-as.vector(mu)*combn(x)
+	
 	
 	mu.tau.phip1.dphin1 = mu*(tau*phi+1)/(phi-1)
 	t.2phin1p1.dtphin1=(tau*(2*phi-1)+1)/tau/(phi-1)
@@ -83,13 +86,46 @@ My_diag_hess<-function(beta, y, x, o, tau, phi)
 	}
 	dmu=(sums1+pos.psi0-neg.psi0)*tau.phip1.dphin1
 	dbetaj.dmuilogl<-as.vector((sums2+pos.psi0-neg.psi0)*(tau.phip1.dphin1)^2)*mu.x
-	
-	diago<-colSums(as.vector(dmu)*dmu2+mu.x*dbetaj.dmuilogl)
-	#offdiag<-
-	diago
+	dmu2.dbi.dbj<-t(as.vector(dmu)*x)%*%(as.vector(mu)*x)
+	Hess<-dmu2.dbi.dbj+(t(dbetaj.dmuilogl)*t(x))%*%(as.vector(mu)*x)
+	Hess
 }
 
-hessian(function(beta)log(my_LBNB(beta=beta, y=10,x=matrix(1:3,nrow=1),o=1, phi=2, tau=.5)), matrix(1:3,nrow=3))
-grad(function(beta)My_score_Beta(beta=beta, y=10, x=matrix(1:3,nrow=1), o=1, tau=.5, phi=2),matrix(1:3,nrow=3))
-My_diag_hess(matrix(1:3,nrow=3), y=10, x=matrix(1:3,nrow=1), o=1, tau=.5, phi=2)
 
+hessian(my_LBNB,x=bet,,,, y=y,x,o=rep(0,10), phi=1+1e-3, tau=2)
+grad(My_score_Beta,x=bet,,,, y=y, x,o=rep(0,10), phi=1+1e-3, tau=2)
+My_hess_beta(beta=bet, y=y,x=x,o=rep(0,10), phi=1+1e-3, tau=2)
+
+betaScore=function(beta, y, phi, tau)-sum(y, My_score_Beta, beta=beta,phi=phi,tau=tau)
+beHess=function(beta, y, phi, tau)-sum(My_hess_beta(y,mu,phi,tau))
+betaNegbnbLogLik=function(beta, y, phi, tau)-sum(my_LBNB(y,beta,phi,tau))
+
+Mlebeta=function(y, phi, tau){
+	
+	par.hess=tryCatch(optim(max(0,10), betaNegbnbLogLik,betaScore,method='L-BFGS-B',lower=-inf, y=y, phi=phi, tau=tau,hessian=TRUE), error=function(...)NA_real_)
+    par=par.hess$par
+	hessian=par.hess$hessian
+	variance=(hessian)
+	var2=My_hess_beta(par, y=y, phi=phi, tau=tau)
+	return(c(mu=par1,var.numerical=variance,
+			 var.theoretical=var2))
+	}
+	
+my.simu<-function(n, beta,phi,tau) {
+mu <- exp(x%*%beta+o)
+	alpha<-(tau*(2*phi-1)+1)/(tau*(phi-1))
+	beta<-1/tau
+	r=mu*(alpha-1)/beta
+	p<-sort(rbeta(n,alpha,beta))
+	y=rnbinom(n,r,prob=p)
+	attr(y, 'phi')=phi
+	attr(y, 'tau')=tau
+	attr(y, 'mu')=mu
+	y
+}	
+	getBnbQnb=function(reps,n, beta, tau, phi)#reps=number of replication
+{replicate(reps,
+	Beta=Mlebeta(my.simu(n, beta, phi, tau), phi, tau)
+      
+)
+}	
