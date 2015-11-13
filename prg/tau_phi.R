2024de17bc91f095b4d0dabe8a7e5c06e54c37cb
Long Qu <long.qu@wright.edu>
2015-11-13 17:11:13 -0500

    simplified gradient for tau and phi

--- prg/tau_phi.R
+++ prg/tau_phi.R
@@ -1,5 +1,3 @@
-
-
 my_LBNB_mu<-function(y,mu,phi,tau)
 {
 	term1 = mu*(tau*phi+1)/(phi-1)
@@ -14,26 +12,35 @@ my_LBNB_mu<-function(y,mu,phi,tau)
 	-sum(log(ans))
 }
 
-dtau=function(y, mu, phi, tau)
+dtau.phi=function(theta, y, mu)
 {
--sum(
-	(1/(tau^2 * (-1 + phi))) * (
-		(-1 + phi) * digamma(1/tau) - 
-		phi * digamma((tau - phi - 2 * tau * phi)/(tau - tau * phi)) - 
-		mu * tau^2 * phi * digamma((mu * (1 + tau * phi))/(-1 + phi)) + 
-		digamma(( 1 - tau + 2 * tau * phi)/(tau * (-1 + phi))) - 
-		digamma(( 1 + mu * tau^2 * phi + tau * (-1 + mu + 2 * phi))/(tau * (-1 + phi))) + 
-		mu * tau^2 * phi * digamma(( 1 + mu * tau^2 * phi + tau * (-1 + mu + 2 * phi))/(tau * (-1 + phi)))
+	phi=theta[1L]; tau=theta[2L]
+	const1=digamma((1-tau+2*tau*phi)/(tau*(phi-1)))
+	const2=digamma((tau-phi-2*tau*phi)/(tau-tau*phi))
+	const3=digamma(1/tau)
 
-		-(-1 + phi) * digamma(y + 1/tau) + 
-		mu * tau^2 * phi * digamma(y + (mu + mu * tau * phi)/(-1 + phi)) + 
-		phi * digamma(y + (phi + mu * tau^2 * phi + tau * (-1 + mu + 2 * phi))/(tau * (-1 + phi))) - 
-		mu * tau^2 * phi * digamma(y + (phi + mu * tau^2 * phi + tau * (-1 + mu + 2 * phi))/(tau * (-1 + phi)))
-	)
-)
-} 
+	dg1=digamma((1+mu*tau^2*phi+tau*(mu-1+2*phi))/(tau*(phi-1)))
+	dg2=digamma((phi+mu*tau^2*phi+tau*(mu-1+y*(phi-1)+2*phi))/(tau*(phi-1)))
+	dg3=digamma((mu*(1+tau*phi))/(phi-1))
+	dg4=digamma(y+(mu+mu*tau*phi)/(phi-1))
+	dg5=digamma(y+1/tau)
 
+	dtau=sum(
+		(1/(tau^2*(phi-1)))*(
+			const1 -dg1
+			+(phi-1)*(const3-dg5)
+			+phi*(dg2-const2)
+			+mu*tau^2*phi*(dg1-dg2-dg3+dg4)
+		))
 
+	dphi=sum(
+		(1/(tau * (phi-1)^2))*(1 + tau) * (
+			dg2 -const2 + const1 -dg1
+			+ mu * tau * (dg3 -dg4 +dg2 -dg1)
+		))
+ 
+	-c(dtau, dphi)
+}
 
 My_phi_score<-function(y, mu,phi,tau)
 {
@@ -62,18 +69,12 @@ My_phi_score<-function(y, mu,phi,tau)
 }
 
 library(numDeriv)
-grad(function(tau)my_LBNB_mu(tau=tau, y=10, phi=2, mu=10.5), .5)
-dtau(y=10, mu=10.5, phi=2, tau=.5)
-
-grad(function(phi)my_LBNB_mu(phi=phi, y=10, tau=.5, mu=10.5), 2)
-My_phi_score(y=10, mu=10.5, phi=2, tau=.5)
-
-
-grad(function(tau)my_LBNB_mu(tau=tau, y=10:20, phi=2, mu=10.5:20.5), .5)
-dtau(y=10:20, mu=10.5:20.5, phi=2, tau=.5)
-
 grad(function(phi)my_LBNB_mu(phi=phi, y=10:20, tau=.5, mu=10.5:20.5), 2)
 My_phi_score(y=10:20, mu=10.5:20.5, phi=2, tau=.5)
+
+grad(function(tau)my_LBNB_mu(tau=tau, y=10:20, phi=2, mu=10.5:20.5), .5)
+grad(function(phi)my_LBNB_mu(phi=phi, y=10:20, tau=.5, mu=10.5:20.5), 2)
+dtau.phi(y=10:20, mu=10.5:20.5, c(phi=2, tau=.5))
 
 ## need: 
 mle=function(y, x, o)
